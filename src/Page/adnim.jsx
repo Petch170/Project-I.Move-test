285924e72c6d1186bbc66bb8f0cfaa8920604d88
09jetsalit <rovesea1@gmail.com>
2024-01-27 12:44:22 +0700

    admin page

---
+++ src/Page/adnim.jsx
@@ -0,0 +1,58 @@
+import React , {useState , useEffect} from "react";
+import Navbarhome from "../Component/Navbarhome";
+import axios from "axios";
+
+
+
+const AddminPage = () => {
+
+    const [members , setMembers] = useState([]);
+
+useEffect(() => {
+    const getDataMembers = async () => {
+        const res = await axios.get("#");
+
+        // setmember
+        if (res.status===200 && res.data) {
+            setMembers([...res.data]);
+        }
+    }
+    getDataMembers();
+
+},[])
+
+
+    return (
+        <div>
+        <Navbarhome />
+        <div>Table Data Members</div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Full Name</th>
+                    <th>E-mail</th>
+                    <th>Phone Number</th>
+                </tr>
+            </thead>
+            <tbody>
+                {members.map((member , id) => {
+
+                    // waiting Data from backend
+
+                    return (
+                        <tr key={`${member} - ${id}`}>
+                            <td>{member.}</td>
+                            <td>{member.}</td>
+                            <td>{member.}</td>
+                            <td>{member.}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table>
+        </div>
+    )
+}
+
+export default AddminPage
