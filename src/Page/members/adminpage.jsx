330b6c11d376f3537e8ddc71ae9b9a1e567017dd
Nan-wp <150341587+Nan-wp@users.noreply.github.com>
2024-01-27 14:40:15 +0700

    fix error

--- src/Page/members/adminpage.jsx
+++ src/Page/members/adminpage.jsx
@@ -1,58 +1,52 @@
-import React , {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Navbarhome from "../../Component/Navbarhome";
 import axios from "axios";
 
-
-
 const AdminPage = () => {
+  const [members, setMembers] = useState([]);
 
-    const [members , setMembers] = useState([]);
-
-useEffect(() => {
+  useEffect(() => {
     const getDataMembers = async () => {
-        const res = await axios.get("#");
+      const res = await axios.get("#");
 
-        // setmember
-        if (res.status===200 && res.data) {
-            setMembers([...res.data]);
-        }
-    }
+      // setmember
+      if (res.status === 200 && res.data) {
+        setMembers([...res.data]);
+      }
+    };
     getDataMembers();
+  }, []);
 
-},[])
+  return (
+    <div>
+      <Navbarhome />
+      <div>Table Data Members</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Full Name</th>
+            <th>E-mail</th>
+            <th>Phone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {members.map((member, id) => {
+            // waiting Data from backend
 
+            return (
+              <tr key={`${member} - ${id}`}>
+                {/* <td>{member.}</td> */}
+                {/* <td>{member.}</td> */}
+                {/* <td>{member.}</td> */}
+                {/* <td>{member.}</td> */}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-    return (
-        <div>
-        <Navbarhome />
-        <div>Table Data Members</div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Full Name</th>
-                    <th>E-mail</th>
-                    <th>Phone Number</th>
-                </tr>
-            </thead>
-            <tbody>
-                {members.map((member , id) => {
-
-                    // waiting Data from backend
-
-                    return (
-                        <tr key={`${member} - ${id}`}>
-                            <td>{member.}</td>
-                            <td>{member.}</td>
-                            <td>{member.}</td>
-                            <td>{member.}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
-        </div>
-    )
-}
-
-export default ddminPage
+export default AdminPage;
