5f5c1b8d99ad3239290c4daa07843e84cb4fe936
Cheartsak <cheartsak555@gmail.com>
2024-02-14 17:52:00 +0700

    change folder name

--- src/Component/index.js
+++ src/Component/index.js
@@ -3,5 +3,5 @@ import Stat from "./Dashboard/Stat";
 import HeaderMobile from "./Settings/HeaderMobile";
 import NavBar from "./Settings/NavBar";
 import SettingAside from "./Settings/SettingAside";
-
+// change path
 export { PieChartComponent, Stat, HeaderMobile, NavBar, SettingAside };
